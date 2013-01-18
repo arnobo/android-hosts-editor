20a08ee93c7371b8b070e5803cefc9c850ab8c93
arnaud.bossmann@gmail.com <arnaud.bossmann@gmail.com@85d05cf0-89d7-5770-355d-91f7c1791d15>
2013-01-18 13:10:22 +0000

    additional tests

--- trunk/HostEditor/src/eu/boss/hosteditor/NewHostActivity.java
+++ trunk/HostEditor/src/eu/boss/hosteditor/NewHostActivity.java
@@ -1,5 +1,6 @@
 package eu.boss.hosteditor;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -34,6 +35,10 @@ public class NewHostActivity extends SherlockActivity implements OnClickListener
 	public void onClick(View v) {
 		switch (v.getId()) {
 		case R.id.btnConfirm:
+			Intent intent = new Intent();
+			intent.putExtra("total", "");
+			setResult(RESULT_OK, intent);
+			finish();
 			break;
 		case R.id.btnCancel:
 			break;
