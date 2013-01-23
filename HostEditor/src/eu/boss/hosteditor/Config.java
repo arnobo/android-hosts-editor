61951d7c31756eb0496cb5d8e3d5830b96417b88
arnaud.bossmann@gmail.com <arnaud.bossmann@gmail.com@85d05cf0-89d7-5770-355d-91f7c1791d15>
2013-01-23 12:34:15 +0000

    Almost all functionalities works

---
+++ HostEditor/src/eu/boss/hosteditor/Config.java
@@ -0,0 +1,13 @@
+package eu.boss.hosteditor;
+
+public class Config {
+	public static final int NEW_HOST = 0;
+	public static final int EDIT_HOST = 1;
+	public static final int RESULT_DELETE = 3;
+
+	public static final String HOST_FILE = "/etc/hosts";
+	public static final String HOST = "HOST";
+	public static final String IP = "IP";
+	public static final String IS_NEW = "IS_NEW";
+
+}
